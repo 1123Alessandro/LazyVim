16f6dcb6f246a74e598778fc3b61638dbfd65dfd
1123Alessandro <alessandroandyrei@gmail.com>
2024-03-03 00:56:53 +0800

    new mappings for terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,10 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
+local util = require("lazyvim.util")
 
 map("n", ";", ":", { desc = "Enter command mode" })
+map("n", "<A-i>", function()
+  util.terminal(nil, { cwd = util.root() })
+end)
+map("t", "<A-i>", "<cmd>close<cr>", { desc = "Hide Terminal" })
